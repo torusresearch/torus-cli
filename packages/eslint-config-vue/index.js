24d1524898aca23ba5c19a074545a85477db6379
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-09-07 15:52:19 +0700

    Add globals in Vue setup

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -14,4 +14,11 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2021,
   },
+  // Workaround https://eslint.vuejs.org/user-guide/#compiler-macros-such-as-defineprops-and-defineemits-are-warned-by-no-undef-rule
+  globals: {
+    defineProps: "readonly",
+    defineEmits: "readonly",
+    defineExpose: "readonly",
+    withDefaults: "readonly"
+  }
 };

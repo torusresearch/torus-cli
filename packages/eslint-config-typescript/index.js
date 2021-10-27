47f941654bd334a0178f8bb5be46ffbf06dd45b0
chaitanyapotti <chaitanya.potti@gmail.com>
2021-10-27 15:56:11 +0800

    add ts eslint

---
+++ packages/eslint-config-typescript/index.js
@@ -0,0 +1,70 @@
+module.exports = {
+  env: {
+    es2020: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "airbnb-typescript/base",
+    "problems",
+    "plugin:@typescript-eslint/recommended",
+    "standard",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:promise/recommended",
+    "plugin:mocha/recommended",
+    "prettier",
+  ],
+  plugins: ["prettier", "promise", "import", "simple-import-sort", "mocha", "@typescript-eslint", "eslint-plugin-tsdoc"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    "tsdoc/syntax": 1,
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "typeLike",
+        format: ["camelCase", "UPPER_CASE", "PascalCase"],
+      },
+    ],
+    "@typescript-eslint/member-ordering": 1,
+    "import/prefer-default-export": 0,
+    "simple-import-sort/imports": 2,
+    "simple-import-sort/exports": 2,
+    "no-dupe-class-members": 0,
+    "@typescript-eslint/no-dupe-class-members": 2,
+    "no-useless-constructor": 0,
+    "@typescript-eslint/no-useless-constructor": 2,
+    "no-unused-vars": 0,
+    "@typescript-eslint/no-unused-vars": ["error", { args: "after-used", argsIgnorePattern: "_" }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        ts: "never",
+      },
+    ],
+    "no-console": 2,
+    "prettier/prettier": [
+      2,
+      {
+        singleQuote: false,
+        printWidth: 150,
+        semi: true,
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".vue"],
+      },
+    },
+  },
+};
--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -1,8 +1,9 @@
 module.exports = {
   env: {
-    es2021: true,
+    es2020: true,
     browser: true,
     node: true,
+    serviceworker: true,
   },
   extends: [
     "plugin:vue/vue3-recommended",
@@ -10,23 +11,41 @@ module.exports = {
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "airbnb-typescript/base",
+    "problems",
+    "plugin:promise/recommended",
+    "plugin:mocha/recommended",
   ],
-  plugins: ["simple-import-sort"],
+  plugins: ["simple-import-sort", "import", "promise", "mocha", "eslint-plugin-tsdoc"],
   parserOptions: {
-    ecmaVersion: 2021,
+    ecmaVersion: 11,
   },
   // Workaround https://eslint.vuejs.org/user-guide/#compiler-macros-such-as-defineprops-and-defineemits-are-warned-by-no-undef-rule
   globals: {
     defineProps: "readonly",
     defineEmits: "readonly",
     defineExpose: "readonly",
-    withDefaults: "readonly"
+    withDefaults: "readonly",
   },
   rules: {
+    "tsdoc/syntax": 1,
+    "@typescript-eslint/member-ordering": 1,
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        ts: "never",
+      },
+    ],
+    "import/prefer-default-export": 0,
     "simple-import-sort/imports": 2,
     "simple-import-sort/exports": 2,
     "no-unused-vars": 0,
-    "@typescript-eslint/no-unused-vars": ["error", { "args": "after-used", "argsIgnorePattern": "_" }],
+    "@typescript-eslint/no-unused-vars": ["error", { args: "after-used", argsIgnorePattern: "_" }],
     "no-console": 2,
     "prettier/prettier": [
       2,
@@ -36,5 +55,5 @@ module.exports = {
         semi: true,
       },
     ],
-  }
+  },
 };

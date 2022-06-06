741f452e0a0fb74f5822f7e76783be6a5ce2f820
chaitanyapotti <chaitanya.potti@gmail.com>
2022-06-06 14:00:39 -0500

    minor bug fix for using plugin n

--- packages/eslint-config-node/index.js
+++ packages/eslint-config-node/index.js
@@ -5,7 +5,7 @@ module.exports = {
     mocha: true,
     commonjs: true,
   },
-  extends: ["@toruslabs/eslint-config-typescript", "plugin:node/recommended", "plugin:security/recommended", "plugin:chai-expect/recommended"],
+  extends: ["@toruslabs/eslint-config-typescript", "plugin:n/recommended", "plugin:security/recommended", "plugin:chai-expect/recommended"],
   plugins: ["chai-expect", "security"],
   parserOptions: {
     ecmaVersion: 11,
@@ -19,14 +19,14 @@ module.exports = {
         ts: "never",
       },
     ],
-    "node/no-unsupported-features/es-syntax": [
+    "n/no-unsupported-features/es-syntax": [
       "error",
       {
-        version: ">=14.17.0",
+        version: ">=16",
         ignores: ["modules"],
       },
     ],
-    "node/no-missing-import": [
+    "n/no-missing-import": [
       "error",
       {
         tryExtensions: [".js", ".json", ".node", ".ts"],

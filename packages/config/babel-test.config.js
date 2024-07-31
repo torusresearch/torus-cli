271015ef43483d294d5953abffd905af97680a7a
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-07-31 15:43:00 +0800

    add babel test config export

---
+++ packages/config/babel-test.config.js
@@ -0,0 +1,14 @@
+const currentPkg = require("./package.json");
+const runtimeVersion = currentPkg.peerDependencies["@babel/runtime"];
+
+module.exports = {
+  presets: [["@babel/env", { bugfixes: true }], "@babel/typescript"],
+  plugins: [
+    "@babel/plugin-syntax-bigint",
+    "@babel/plugin-transform-object-rest-spread",
+    "@babel/plugin-transform-class-properties",
+    ["@babel/transform-runtime", { version: runtimeVersion }],
+    "@babel/plugin-transform-numeric-separator",
+  ],
+  sourceType: "unambiguous",
+};

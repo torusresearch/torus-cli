a13fe0577460951ab68bb1b61fd2f144832c824a
hieu-w <hieu@tor.us>
2024-12-10 16:29:50 +0700

    eslint-config-node: update eslint9 flat config

--- packages/eslint-config-node/index.js
+++ packages/eslint-config-node/index.js
@@ -1,32 +1,43 @@
-module.exports = {
-  env: {
-    es2020: true,
-    node: true,
-    mocha: true,
-    commonjs: true,
-  },
-  extends: ["@toruslabs/eslint-config-typescript", "plugin:n/recommended", "plugin:security/recommended-legacy", "plugin:chai-expect/recommended"],
-  plugins: ["chai-expect", "security"],
-  parserOptions: {
-    ecmaVersion: 11,
-  },
-  rules: {
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        js: "never",
-        ts: "never",
-      },
-    ],
-    "n/no-unsupported-features/es-syntax": [
-      "error",
-      {
-        version: ">=16",
-        ignores: ["modules"],
-      },
-    ],
-    "n/no-missing-import": 0,
-    "import/no-extraneous-dependencies": ["error", { devDependencies: ["**/*.test.ts", "**/*.test.tsx", "**/testHelpers.ts"] }],
-  },
-};
+import chaiExpectPlugin from 'eslint-plugin-chai-expect';
+import nodePlugin from "eslint-plugin-n";
+import pluginSecurity from 'eslint-plugin-security';
+import torusTypescriptConfig from "@toruslabs/eslint-config-typescript";
+import globals from "globals";
+
+export default [
+    ...torusTypescriptConfig,
+    chaiExpectPlugin.configs["recommended-flat"],
+    nodePlugin.configs["flat/recommended-script"],
+    pluginSecurity.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.mocha,
+                ...globals.commonjs,
+                ...globals.es2020,
+            },
+
+            ecmaVersion: 11,
+            sourceType: "module",
+        },
+
+        rules: {
+            "import/extensions": ["error", "ignorePackages", {
+                js: "never",
+                ts: "never",
+            }],
+
+            "n/no-unsupported-features/es-syntax": ["error", {
+                version: ">=16",
+                ignores: ["modules"],
+            }],
+
+            "n/no-missing-import": 0,
+
+            "import/no-extraneous-dependencies": ["error", {
+                devDependencies: ["**/*.test.ts", "**/*.test.tsx", "**/testHelpers.ts"],
+            }],
+        },
+    }
+];

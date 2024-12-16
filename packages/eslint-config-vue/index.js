aea5c3b8f811dc775780de8d3f7694000cf94f10
hieu-w <hieu@tor.us>
2024-12-17 00:51:52 +0700

    eslint-config-vue: update deps

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -1,54 +1,53 @@
-module.exports = {
-  env: {
-    es2020: true,
-    browser: true,
-    node: true,
-  },
-  extends: [
-    "problems",
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "@vue/airbnb",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:promise/recommended",
-    "prettier",
-  ],
-  plugins: ["prettier", "promise", "import", "simple-import-sort", "eslint-plugin-tsdoc"],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  // Workaround https://eslint.vuejs.org/user-guide/#compiler-macros-such-as-defineprops-and-defineemits-are-warned-by-no-undef-rule
-  globals: {
-    defineProps: "readonly",
-    defineEmits: "readonly",
-    defineExpose: "readonly",
-    withDefaults: "readonly",
-  },
-  rules: {
-    "tsdoc/syntax": 1,
-    "@typescript-eslint/member-ordering": 1,
-    "import/prefer-default-export": 0,
-    "simple-import-sort/imports": 2,
-    "simple-import-sort/exports": 2,
-    "no-dupe-class-members": 0,
-    "@typescript-eslint/no-dupe-class-members": 2,
-    "no-useless-constructor": 0,
-    "@typescript-eslint/no-useless-constructor": 2,
-    "no-unused-vars": 0,
-    "@typescript-eslint/no-unused-vars": ["error", { args: "after-used", argsIgnorePattern: "_" }],
-    "no-console": 2,
-    "prettier/prettier": [
-      2,
-      {
-        singleQuote: false,
-        printWidth: 150,
-        semi: true,
-        trailingComma: "es5",
-      },
-    ],
-  },
-};
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import torusTypescriptConfig from "@toruslabs/eslint-config-typescript";
+
+export default [
+    ...torusTypescriptConfig,
+    ...pluginVue.configs['flat/recommended'],
+    ...vueTsEslintConfig(),
+    {
+        plugins: {},
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                defineProps: "readonly",
+                defineEmits: "readonly",
+                defineExpose: "readonly",
+                withDefaults: "readonly",
+            },
+
+            ecmaVersion: 2020,
+            sourceType: "commonjs",
+        },
+
+        rules: {
+            "tsdoc/syntax": 1,
+            "@typescript-eslint/member-ordering": 1,
+            "import/prefer-default-export": 0,
+            "simple-import-sort/imports": 2,
+            "simple-import-sort/exports": 2,
+            "no-dupe-class-members": 0,
+            "@typescript-eslint/no-dupe-class-members": 2,
+            "no-useless-constructor": 0,
+            "@typescript-eslint/no-useless-constructor": 2,
+            "no-unused-vars": 0,
+
+            "@typescript-eslint/no-unused-vars": ["error", {
+                args: "after-used",
+                argsIgnorePattern: "_",
+            }],
+
+            "no-console": 2,
+
+            "prettier/prettier": [2, {
+                singleQuote: false,
+                printWidth: 150,
+                semi: true,
+                trailingComma: "es5",
+            }],
+        },
+    }];

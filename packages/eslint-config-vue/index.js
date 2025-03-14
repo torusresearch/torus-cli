a1d062783c1474c8d4834c6738af8b846f0cc076
hieu-w <hieu@tor.us>
2025-03-14 22:01:59 +0700

    Fix vue eslint issues

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -5,10 +5,16 @@ import torusTypescriptConfig from "@toruslabs/eslint-config-typescript";
 import tailwind from "eslint-plugin-tailwindcss";
 import vueParser from "vue-eslint-parser";
 
+// Remove the base/setup and base/setup-for-vue configs from the pluginVue config
+// These items are already exist in pluginVue.configs["flat/recommended"]
+// This cause issue when we redefine vue() plugin
+const duplicateConfigs = ["vue/base/setup", "vue/base/setup-for-vue"];
+const pluginVueConfig = pluginVue.configs["flat/recommended"].filter((config) => !duplicateConfigs.includes(config.name));
+
 // TODO: Add back eslint-vue when it's ready
 export default [
   ...torusTypescriptConfig,
-  ...defineConfigWithVueTs(pluginVue.configs["flat/recommended"], vueTsConfigs.recommended),
+  ...defineConfigWithVueTs(pluginVueConfig, vueTsConfigs.recommended),
   ...tailwind.configs["flat/recommended"],
   {
     plugins: {},
@@ -36,8 +42,6 @@ export default [
     rules: {
       "@typescript-eslint/member-ordering": 1,
       "import/prefer-default-export": 0,
-      "simple-import-sort/imports": 2,
-      "simple-import-sort/exports": 2,
       "no-dupe-class-members": 0,
       "@typescript-eslint/no-dupe-class-members": 2,
       "no-useless-constructor": 0,

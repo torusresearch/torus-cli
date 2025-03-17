8bfb09615fb802fcec4b3d77554e0c0b17bcf241
Chaitanya Potti <chaitanya.potti@gmail.com>
2025-03-17 11:26:44 +0800

    add some vue html rules

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -40,6 +40,8 @@ export default [
     },
 
     rules: {
+      "vue/html-self-closing": 0,
+      "vue/max-attributes-per-line": 0,
       "vue/multi-word-component-names": 0,
       "@typescript-eslint/member-ordering": 1,
       "import/prefer-default-export": 0,
